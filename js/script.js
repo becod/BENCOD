6caf3444add144bcf07952d6e1fc97fa2dab534d
Bencod <benyamin.ak@hotmail.com>
2017-08-13 19:51:34 +0200

    Cambios importantes
    Hubicacion de texto
    Brand name crated
    Favicon introduced
    y menu terminado

--- js/script.js
+++ js/script.js
@@ -21,3 +21,21 @@ $(function() {
         //$(".text").css({"margin-top":"-" + Num3mod + "px"});
     });
 }.call(this));
+
+var nav = document.getElementById('container-nav');
+var navlinks = nav.getElementsByTagName('a');
+
+function toggleNav() {
+    (nav.classList.contains('active')) ? nav.classList.remove('active') : nav.classList.add('active');
+  }
+
+document.getElementById('nav-icon').addEventListener('click', function(e) {
+    e.preventDefault();
+    toggleNav();
+});
+
+for(var i = 0; i < navlinks.length; i++) {
+    navlinks[i].addEventListener('click', function() {
+      toggleNav();
+  });
+}
---
+++ src/peices/contact-me-animation-concept (1)/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/nizarmah/pen/ZQqmQr.
+
+ Another day, working on my website when I get carried away try to make fun CSS Animations.
